5898ed8a4962ce00b425d46ce4c6f2f7708cacfe
Alyssa Jane <xcthaeh@gmail.com>
2019-05-28 22:31:10 -0400

    added cors to router file (stretch goal)

---
+++ router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const db = require("./data/db");
+const cors = require("cors");
+
+router.use(express.json());
+server.use(cors());
+
