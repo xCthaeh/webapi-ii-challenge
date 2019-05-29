7d495d34a7c1e8c224ea6e5b4b2d26e274ab5f8f
Alyssa Jane <xcthaeh@gmail.com>
2019-05-28 22:32:22 -0400

    POST request created

--- router.js
+++ router.js
@@ -6,3 +6,22 @@ const cors = require("cors");
 router.use(express.json());
 server.use(cors());
 
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post."
+    });
+  } else {
+    db.insert({ title, contents })
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          message: "There was an error while saving the post to the database."
+        });
+      });
+  }
+});
